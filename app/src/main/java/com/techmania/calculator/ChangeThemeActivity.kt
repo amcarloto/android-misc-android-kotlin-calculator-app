f9cabadf153a956b772a2f090cb982d9a5ce72ee
Mehmet <mehmetong45@gmail.com>
2023-09-25 11:23:27 +0300

    Firs commit for Calculator App

---
+++ app/src/main/java/com/techmania/calculator/ChangeThemeActivity.kt
@@ -0,0 +1,52 @@
+package com.techmania.calculator
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.techmania.calculator.databinding.ActivityChangeThemeBinding
+
+class ChangeThemeActivity : AppCompatActivity() {
+
+    lateinit var switchBinding : ActivityChangeThemeBinding
+    lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        switchBinding = ActivityChangeThemeBinding.inflate(layoutInflater)
+        val view = switchBinding.root
+        setContentView(view)
+
+        switchBinding.toolbar2.setNavigationOnClickListener {
+            finish()
+        }
+
+        switchBinding.mySwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            sharedPreferences = this.getSharedPreferences("Dark Theme",Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+
+            if (isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("switch",true)
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("switch",false)
+            }
+
+            editor.apply()
+
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sharedPreferences = this.getSharedPreferences("Dark Theme",Context.MODE_PRIVATE)
+        val isDark = sharedPreferences.getBoolean("switch",false)
+        switchBinding.mySwitch.isChecked = isDark
+
+    }
+}
